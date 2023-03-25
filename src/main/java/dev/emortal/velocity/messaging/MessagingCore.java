8425b4199cdda720d67c097f433416c4e1008a69
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-03-25 04:26:08 +0000

    Publish connections to kafka

--- src/main/java/dev/emortal/velocity/messaging/MessagingCore.java
+++ src/main/java/dev/emortal/velocity/messaging/MessagingCore.java
@@ -1,12 +1,21 @@
 package dev.emortal.velocity.messaging;
 
 import com.google.protobuf.AbstractMessage;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.proxy.ProxyServer;
-import dev.emortal.api.utils.kafka.KafkaCore;
+import dev.emortal.api.message.common.PlayerConnectMessage;
+import dev.emortal.api.message.common.PlayerDisconnectMessage;
+import dev.emortal.api.message.common.PlayerSwitchServerMessage;
+import dev.emortal.api.utils.kafka.FriendlyKafkaConsumer;
+import dev.emortal.api.utils.kafka.FriendlyKafkaProducer;
 import dev.emortal.api.utils.kafka.KafkaSettings;
 import dev.emortal.api.utils.parser.MessageProtoConfig;
 import dev.emortal.api.utils.parser.ProtoParserRegistry;
 import dev.emortal.velocity.CorePlugin;
+import dev.emortal.velocity.Environment;
 
 import java.util.function.Consumer;
 
@@ -14,33 +23,71 @@ public class MessagingCore {
     private static final String KAFKA_HOST = System.getenv("KAFKA_HOST");
     private static final String KAFKA_PORT = System.getenv("KAFKA_PORT");
 
+    private static final String KAFKA_CONNECTIONS_TOPIC = "mc-connections";
+
     private final RabbitMqCore rabbitMqCore;
-    private final KafkaCore kafkaCore;
+    private final FriendlyKafkaConsumer kafkaConsumer;
+    private final FriendlyKafkaProducer kafkaProducer;
 
     public MessagingCore(ProxyServer proxy, CorePlugin plugin) {
         this.rabbitMqCore = new RabbitMqCore();
-        this.kafkaCore = new KafkaCore(
-                KafkaSettings.builder()
-                        .autoCommit(true)
-                        .bootstrapServers(KAFKA_HOST + ":" + KAFKA_PORT).build()
-        );
+
+        KafkaSettings kafkaSettings = new KafkaSettings()
+                .setAutoCommit(true)
+                .setBootstrapServers(KAFKA_HOST + ":" + KAFKA_PORT);
+
+        this.kafkaConsumer = new FriendlyKafkaConsumer(kafkaSettings);
+        this.kafkaProducer = new FriendlyKafkaProducer(kafkaSettings);
 
         // Register RabbitMQ events
         proxy.getEventManager().register(plugin, this.rabbitMqCore);
+        // Register Kafka events
+        proxy.getEventManager().register(plugin, this);
     }
 
     public <T extends AbstractMessage> void setListener(Class<T> messageType, Consumer<T> listener) {
         MessageProtoConfig<T> parser = ProtoParserRegistry.getParser(messageType);
 
         switch (parser.service()) {
-            case KAFKA -> this.kafkaCore.setListener(messageType, listener);
+            case KAFKA -> this.kafkaConsumer.setListener(messageType, listener);
             case RABBIT_MQ -> this.rabbitMqCore.setListener(messageType, listener);
             default -> throw new IllegalStateException("Unexpected value: " + parser.service());
         }
     }
 
+    // Kafka producing
+    @Subscribe
+    public void onPlayerLogin(PostLoginEvent event) {
+        PlayerConnectMessage message = PlayerConnectMessage.newBuilder()
+                .setPlayerId(event.getPlayer().getUniqueId().toString())
+                .setPlayerUsername(event.getPlayer().getUsername())
+                .setServerId(Environment.getHostname())
+                .build();
+
+        this.kafkaProducer.produce(KAFKA_CONNECTIONS_TOPIC, message);
+    }
+
+    @Subscribe
+    public void onPlayerDisconnect(DisconnectEvent event) {
+        PlayerDisconnectMessage message = PlayerDisconnectMessage.newBuilder()
+                .setPlayerId(event.getPlayer().getUniqueId().toString())
+                .build();
+
+        this.kafkaProducer.produce(KAFKA_CONNECTIONS_TOPIC, message);
+    }
+
+    @Subscribe
+    public void onServerSwitch(ServerConnectedEvent event) {
+        PlayerSwitchServerMessage message = PlayerSwitchServerMessage.newBuilder()
+                .setPlayerId(event.getPlayer().getUniqueId().toString())
+                .setServerId(event.getServer().getServerInfo().getName())
+                .build();
+
+        this.kafkaProducer.produce(KAFKA_CONNECTIONS_TOPIC, message);
+    }
+
     public void shutdown() {
         this.rabbitMqCore.shutdown();
-        this.kafkaCore.shutdown();
+        this.kafkaConsumer.close();
     }
 }
