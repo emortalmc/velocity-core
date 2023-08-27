e7589d4a3c5747377aed857999d6894e93b2da34
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-08-27 20:48:21 +0100

    Fixed leave command being registered with player argument

--- src/main/java/dev/emortal/velocity/party/commands/PartyCommand.java
+++ src/main/java/dev/emortal/velocity/party/commands/PartyCommand.java
@@ -31,7 +31,7 @@ public final class PartyCommand extends EmortalCommand {
         var playerArgument = argument("player", StringArgumentType.word(), usernameSuggesterProvider.online());
         super.addSyntax(new PartyInviteSub(partyService, playerResolver), literal("invite"), playerArgument);
         super.addSyntax(new PartyJoinSub(partyService, playerResolver), literal("join"), playerArgument);
-        super.addSyntax(new PartyLeaveSub(partyService), literal("leave"), playerArgument);
+        super.addSyntax(new PartyLeaveSub(partyService), literal("leave"));
         super.addSyntax(new PartyKickSub(partyService, playerResolver), literal("kick"), playerArgument);
         super.addSyntax(new PartyLeaderSub(partyService, playerResolver), literal("leader"), playerArgument);
         super.addSyntax(new PartyDisbandSub(partyService), literal("disband"));
